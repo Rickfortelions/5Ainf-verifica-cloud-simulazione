8020dfafa82f827f56aa220e440764e64f1722e0
Riccardo <riccardoforteleoni@hotmail.it>
2019-04-10 13:20:26 +0200

    da rick

--- code/js/app.js
+++ code/js/app.js
@@ -1,15 +1,29 @@
 var app = {
-	init: function() {
+	init: function () {
 		console.log("init");
 		$.getJSON("api/data.json")
-		.done(app.showCards)
-		.fail(app.onError);
+			.done(app.showCards)
+			.fail(app.onError);
 	},
 	showCards(dataJson) {
 		console.log(dataJson);
-		
+		jsonData.cardList.forEach(element => {
+			let card = `<div class="col-lg-3 col-md-6 mb-4 net-all">
+			<div class="card h-100">
+			  <img class="card-img-top" src="${element.imgUrl}" alt="Moodle">
+			  <div class="card-body">
+				<h4 class="card-title">${element.title}</h4>
+				<p class="card-text">${element.desc}</p>
+			  </div>
+			  <div class="card-footer">
+				<a href="${element.buttonUrl}" class="btn btn-primary">Accedi</a>
+			  </div>
+			</div>
+		  </div>`;
+			$("#cardCointaner").append(card);
+		})
 	},
-	onError: function(e) {
+	onError: function (e) {
 		console.log(`Ajax error: ${JSON.stringify(e)}`);
 	}
 
